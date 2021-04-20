1c70fe55f426b3bb690c22ecff32987623eb103d
cesnopod2 <62251975+cesnopod2@users.noreply.github.com>
2021-04-20 23:42:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,Request,status
 
+from hashlib import sha512
 app=FastAPI()
 @app.get("/") # dekorator
 def root():
     return {"message": "Hello world!"}
+
+@app.get("/auth")
+def auth_view(response:Response, password: str =None , pass_hash= str=None):
+    response.status_code=status.HTTP_401_UNAUTHORIZED
+    if not(password and pass_hash):
+        return 
+    if sha512(password.encode()).hexdigest() == pass_hash:
+        response.status_code=status.HTTP_204_NO_CONTENT
